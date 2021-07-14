a8f8efa9268ba5dcae15562048027cae6559d3f0
UKMO-lsampson <lewis.sampson@metoffice.gov.uk>
2021-07-14 12:58:57 +0000

    Preparing testing framework for merging back into master and develop, creating a consistent stage of running the miniapp.

--- gpu_srctrm_test/RUN/clean.sh
+++ gpu_srctrm_test/RUN/clean.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
-rm -f ww3_*
+rm -f *ww3*
 rm -f out/*.ww3
 rm -f out/*.err
 rm -f out/*.out
+rm -fr out/ww3_outp/
 rm -fr out/ww3_outf/
 rm -f ST4TABUHF2.bin
 rm -f run_shel.time
-
+rm -f *err*
+rm -f prof
+rm -f temp
